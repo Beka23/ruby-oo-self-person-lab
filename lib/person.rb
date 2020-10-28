7af66865fec0e129ca2e9a2534cfa5ba7a3dbff8
Beka23 <beksultankaiypov@gmail.com>
2020-10-28 11:18:13 -0400

    commit tree 3b250e0adaa83d63a3586a7de366a50bd3fa17ce

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
 # your code goes here
+
+
+ class Person
+    attr_reader :name 
+    attr_writer :bank_account
+
+    @@bank_account = 25
+
+    def initialize(name)
+        @name = name 
+    end
+    
+    def bank_account
+        @bank_account
+    end
+
+ end
+
+
+
